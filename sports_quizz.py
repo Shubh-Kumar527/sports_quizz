5a731009347e3c5802a796514e661fa4773ad190
Shubh-Kumar527 <shubhstudios527@gmail.com>
2025-08-02 10:04:17 +0530

    Update sports_quizz.py

--- sports_quizz.py
+++ sports_quizz.py
@@ -109,7 +109,7 @@ mcq_questions = [
 ]
 
 # Free-text Quiz Mode
-if mode == "Multiple Choice":
+if mode == "short answer questions":
     if 'asked_questions' not in st.session_state:
         st.session_state.asked_questions = []
     if 'question' not in st.session_state or st.session_state.question == "":
